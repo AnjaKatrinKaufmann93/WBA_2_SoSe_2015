b524f5c90f3243ff9d4a5336ea9dcb6207d13b28
Anja_Kaufmann <anja@Anjas-MBP.fritz.box>
2015-04-15 16:26:07 +0200

    change Aufgabe 1

--- aufgabe_1.js
+++ aufgabe_1.js
@@ -1,6 +1,12 @@
 var fs = require('fs');
+var data;
 fs.readFile("Wolkenkratzer.json", function(err, data){
   if(err) throw err;
-
   
+  data =JSON.parse(data);
+  data = JSON.stringify(data);
+
+  console.log(data);
+
+
 });
