985beefc8439c3ce908f827c66b1037a423603b7
Anja_Kaufmann <anja@Anjas-MBP.fritz.box>
2015-05-02 16:45:53 +0200

    Aufgabe_3 hinzugefügt

--- aufgabe_1.js
+++ aufgabe_1.js
@@ -2,6 +2,8 @@ var fs = require('fs');
 var chalk = require('chalk');
 
 var body;
+var data_sorted;
+
 
 fs.readFile("Wolkenkratzer.json", function(err, data){
   if(err) throw err;
@@ -15,6 +17,23 @@ fs.readFile("Wolkenkratzer.json", function(err, data){
     console.log(body.wolkenkratzer[i].hoehe);
     console.log("-----------------------");
   }
+
+  body.wolkenkratzer.sort(function (a,b){
+    return a.hoehe-b.hoehe;
+  });
+
+  data_sorted = JSON.stringify(body);
+
+  fs.writeFile("wolkenkratzer_sortiert.json",data_sorted, function(err) {
+    if(err) throw err;
+    for (var i=0; i<body.wolkenkratzer.length; i++){
+      console.log(chalk.blue(body.wolkenkratzer[i].name));
+      console.log(body.wolkenkratzer[i].stadt);
+      console.log(body.wolkenkratzer[i].hoehe);
+      console.log("-----------------------");
+    }
+  });
+
 
 
 
